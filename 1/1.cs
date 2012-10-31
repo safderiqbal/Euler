e0ff6e11680f15e6568b6f9f47082b687b0eff3f
Safder <safder.iqbal@gmail.com>
2012-10-31 15:38:55 +0000

    Completed problem 1
    http://projecteuler.net/thread=1

--- 1/1.cs
+++ 1/1.cs
@@ -5,10 +5,20 @@ using System.Text;
 
 namespace _1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class _1
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Project Euler\n\nProblem 1");
+			int total = 0;
+			for (int i = 1; i < 1000; i++)
+				if (i % 3 == 0 || i % 5 == 0)
+				{
+					total += i;
+				}
+			Console.WriteLine("Sum of all multiples = " + total);
+			Console.WriteLine("Press any key to exit.");
+			Console.ReadKey();
+		}
+	}
 }
