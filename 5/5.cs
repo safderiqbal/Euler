28b80e942c99cabbdc8fb2c3c1032ac0435ba133
Safder <safder.iqbal@gmail.com>
2012-11-07 17:34:47 +0000

    Initial commit of Problem 5

---
+++ 5/5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _5
+{
+	class _5
+	{
+		/// <summary>
+		/// http://projecteuler.net/problem=5
+		/// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+		/// What is the smallest positive number that is evenly divisible (divisible with no remainder) by all of the numbers from 1 to 20?
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
