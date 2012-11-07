f0e4c44190b776fbd45b5391a98296a4eccb2587
Safder <safder.iqbal@gmail.com>
2012-11-07 15:23:47 +0000

    Initial commit, problem 3

---
+++ 3/3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3
+{
+	class _3
+	{
+		/// <summary>
+		/// http://projecteuler.net/problem=3
+		/// The prime factors of 13195 are 5, 7, 13 and 29.
+		/// What is the largest prime factor of the number 600851475143 ?
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
