70d5986be4c0a3ab9ed637d91738d3d20d2921a4
Safder <safder.iqbal@gmail.com>
2012-11-07 16:43:14 +0000

    Completed Problem 4 http://projecteuler.net/thread=4

--- 4/4.cs
+++ 4/4.cs
@@ -16,6 +16,58 @@ namespace _4
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Project Euler\n\nProblem 4");
+			_4 main = new _4();
+			// Console.WriteLine("101 is " + (main.isPalindrome(101) ? "" : "not ") + "a palindrome.");		//Test
+			int digit = 3;
+			int result = main.loop(main, digit);
+			if (result == 0)
+				Environment.Exit(-1);		// Should not reach here.
+
+			Console.WriteLine("The largest palindrome made of two " + digit + " digit numbers is " + result);
+
+			Console.ReadKey();
+		}
+
+		public Boolean isPalindrome(int number)
+		{
+			int orig = number;
+			int reverse = 0;
+			int digit;
+
+			while (number > 0)
+			{
+				digit = number % 10;
+				reverse = reverse * 10 + digit;
+				number = number /= 10;
+			}
+
+			if (reverse == orig)
+				return true;
+			else
+				return false;
+		}
+
+		public int loop(_4 main, int digits)
+		{
+			double start = 0;
+			for (int i = digits-1; i >= 0; i--)
+			{
+				start += Math.Pow(10, i) * 9;
+			}
+
+			int value1, value2;
+			for (value1 = (int)start; value1 > 900; value1--)
+			{
+				for (value2 = (int)start; value2 > 900; value2--)
+				{
+					if (main.isPalindrome(value1 * value2))
+					{
+						Console.WriteLine(value1 + ":" + value2);
+						return value1 * value2;
+					}
+				}
+			}
+			return 0;
 		}
 	}
 }
